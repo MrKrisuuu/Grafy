94e55b13070ff3eea202ac81695ccb66d4ebc2a5
JanickiJ <kubajanicki244@gmail.com>
2023-12-22 17:43:45 +0100

    added test with extra edge

--- group_3/start_graphs_3.py
+++ group_3/start_graphs_3.py
@@ -4,6 +4,7 @@ from helpers import cut_edge
 
 def get_P5_left():
     G = create_graph_4()
+
     v3 = list(G.nodes)[0]
     v2 = list(G.nodes)[1]
     v1 = list(G.nodes)[2]
@@ -12,12 +13,15 @@ def get_P5_left():
     e23 = list(G.nodes)[4]
     e12 = list(G.nodes)[5]
     e14 = list(G.nodes)[6]
+    e43 = list(G.nodes)[7]
 
     def f(ve1, ve2, ee12):
         v = cut_edge(G, ve1, ve2, ee12)
         v.h = True
         G.nodes[v]['h'] = True
 
+    # e43.b = False
+    # G.nodes[e43]['b'] = False
     f(v3, v2, e23)
     f(v1, v2, e12)
     f(v1, v4, e14)
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import random
 
 from Node import NodeV, NodeE, NodeQ
+from helpers import add_node, add_edge
 from utils import draw_graph
 
 
@@ -73,6 +74,29 @@ def change_r_test(get_left, P):
         print(f"change_r_test FAILED")
         return 0
 
+def add_node_test(get_left, P):
+    G = get_left()
+
+    new_v = NodeV(2, 2, False)
+
+    add_node(G, new_v)
+
+    v0 = list(G.nodes)[0]
+    v1 = list(G.nodes)[1]
+
+    add_edge(G, v0, new_v, True)
+    add_edge(G, v1, new_v, True)
+
+    draw_graph(G)
+
+    if P.apply(G):
+        print(f"add_node_test OK")
+        draw_graph(G)
+        return 1
+    else:
+        draw_graph(G)
+        print(f"add_node_test FAILED")
+        return 0
 
 def do_tests(get_left, P):
     OK = 0
@@ -81,4 +105,5 @@ def do_tests(get_left, P):
     OK += remove_edge_test(get_left, P)
     OK += check_coordinates_test(get_left, P)
     OK += change_r_test(get_left, P)
-    print(f"{OK}/5")
+    OK += add_node_test(get_left, P)
+    print(f"{OK}/6")
