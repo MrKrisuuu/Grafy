ddec0fa5e3a1cd78a1f62b4ab1b2ddbcb95487cb
mmisiak <mmisiak@qualtrics.com>
2024-01-11 22:53:37 +0100

    add P8 with tests & fix coordinates test

--- group_3/productions_3.py
+++ group_3/productions_3.py
@@ -1,5 +1,27 @@
-from group_3.start_graphs_3 import get_P6_left, get_P5_left
+from group_3.start_graphs_3 import get_P6_left, get_P5_left, get_P8_left
 from productions import Production
+from networkx.algorithms.isomorphism import ISMAGS, GraphMatcher
+from Node import NodeQ
+from productions import node_match
+
+
 
 P5 = Production(get_P5_left())
 P6 = Production(get_P6_left())
+
+class P8(Production):
+    def __init__(self):
+        self.left = get_P8_left()
+
+    def apply(self, graph):
+        matcher = GraphMatcher(graph, self.left, node_match=node_match)
+        for subgraph_nodes in matcher.subgraph_isomorphisms_iter():
+            isomorphic_subgraph = graph.subgraph(subgraph_nodes)
+            for node in isomorphic_subgraph.nodes:
+                if isinstance(node, NodeQ):
+                    graph.nodes[node]["r"] = True
+                    node.r = True
+            return True
+        return False
+
+P8 = P8()
--- group_3/start_graphs_3.py
+++ group_3/start_graphs_3.py
@@ -1,5 +1,7 @@
 from examples import create_graph_4
-from helpers import cut_edge
+from helpers import cut_edge, add_edge, add_hyperedge, add_node, cut_edge
+import networkx as nx
+from Node import NodeV, NodeE, NodeQ
 
 
 def get_P5_left():
@@ -50,5 +52,32 @@ def get_P6_left():
     f(v1, v2, e12)
     f(v1, v4, e14)
     f(v4, v3, e43)
+
+    return G
+
+def get_P8_left():
+    v1 = NodeV(0, 0, False)  
+    v2 = NodeV(1, 0, False)
+    v3 = NodeV(1, 1, False)
+    v4 = NodeV(0, 1, False)
+    v5 = NodeV(1, 0.5, True)
+    v6 = NodeV(2, 0.5, False)
+    v7 = NodeV(2, 1, False)
+
+    G = nx.Graph()
+
+    add_node(G, v1)
+    add_node(G, v2)
+    add_node(G, v3)
+    add_node(G, v4)
+    add_node(G, v5)
+    add_node(G, v6)
+    add_node(G, v7)
+
+    add_edge(G, v3, v5, False)
+    add_edge(G, v5, v2, False)
+
+    add_hyperedge(G, [v3, v7, v6, v5], True)
+    add_hyperedge(G, [v1, v2, v3, v4], False)
 
     return G
