c85beaa1c28a8e1362dea92e4008577fb09acaaa
MrKrisuuu <krisuuu57@gmail.com>
2024-01-19 17:09:39 +0100

    drawing

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 from Node import NodeV, NodeE, NodeQ
 
@@ -14,12 +15,22 @@ def get_positions(G):
 def draw_graph(G):
     pos = get_positions(G)
     colors = []
-    for node in G.nodes:
+    removes = []
+    tmp = deepcopy(G)
+    pos = get_positions(tmp)
+    for node in tmp.nodes:
+        if isinstance(node, NodeQ):
+            for edge in tmp.edges:
+                if node in edge:
+                    removes.append(edge)
+    for edge in removes:
+        tmp.remove_edge(*edge)
+    for node in tmp.nodes:
         if isinstance(node, NodeV):
             colors.append("red")
         if isinstance(node, NodeE):
             colors.append("yellow")
         if isinstance(node, NodeQ):
             colors.append("green")
-    nx.draw(G, pos=pos, with_labels=True, node_color=colors)
+    nx.draw(tmp, pos=pos, with_labels=True, node_color=colors, node_size=10, font_size=10)
     plt.show()
