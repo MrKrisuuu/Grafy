9e378e0ebff78331816c2a2a57d13ce20aec2380
MikolajKardys <mikolaj.kardys@gmail.com>
2024-01-19 17:59:48 +0100

    Flow done

--- group_3/productions_3.py
+++ group_3/productions_3.py
@@ -2,7 +2,7 @@ from networkx.algorithms.isomorphism import GraphMatcher
 
 from Node import NodeQ
 from group_3.start_graphs_3 import get_P6_left, get_P5_left, get_P8_left
-from productions import Production
+from productions import Production, node_match_p7
 from productions import node_match
 
 P5 = Production(get_P5_left())
@@ -11,12 +11,14 @@ P6 = Production(get_P6_left())
 
 class P8(Production):
     def __init__(self):
-        self.left = get_P8_left()
+        super().__init__(get_P8_left())
 
-    def apply(self, graph):
-        matcher = GraphMatcher(graph, self.left, node_match=node_match)
+    def apply(self, graph, node_q=None):
+        matcher = GraphMatcher(graph, self.left, node_match=node_match_p7)
         for subgraph_nodes in matcher.subgraph_isomorphisms_iter():
             isomorphic_subgraph = graph.subgraph(subgraph_nodes)
+            if node_q and node_q not in isomorphic_subgraph.nodes:
+                continue
             for node in isomorphic_subgraph.nodes:
                 if isinstance(node, NodeQ):
                     graph.nodes[node]["r"] = True
